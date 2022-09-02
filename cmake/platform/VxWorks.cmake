20024f24619aae900490e4672233fc0c73eb2906
Kevin F Ortega <kevin.f.ortega@jpl.nasa.gov>
2022-09-01 18:59:16 -0700

    including correct types

--- cmake/platform/VxWorks.cmake
+++ cmake/platform/VxWorks.cmake
@@ -6,4 +6,4 @@ add_definitions(-D_WRS_KERNEL)
 set(FPRIME_USE_BAREMETAL_SCHEDULE OFF)
 
 # Add VxWorks specific headers into the system
-include_directories(SYSTEM "${FPRIME_FRAMEWORK_PATH}/Fw/Types/VxWorks")
+include_directories(SYSTEM "${CMAKE_CURRENT_LIST_DIR}/types")
