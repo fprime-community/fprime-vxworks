e2cbb76cf35dd675a465325bafca379356fb35a3
Kevin F Ortega <kevin.f.ortega@jpl.nasa.gov>
2022-09-07 14:05:36 -0700

    including types based on vxworks version

--- cmake/platform/VxWorks.cmake
+++ cmake/platform/VxWorks.cmake
@@ -6,4 +6,9 @@ add_definitions(-D_WRS_KERNEL)
 set(FPRIME_USE_BAREMETAL_SCHEDULE OFF)
 
 # Add VxWorks specific headers into the system
-include_directories(SYSTEM "${CMAKE_CURRENT_LIST_DIR}/types")
+set(VXWORKS_TYPES "${CMAKE_CURRENT_LIST_DIR}/types/VxWorks${CMAKE_SYSTEM_VERSION}")
+IF(NOT EXISTS "${VXWORKS_TYPES}")
+    message(FATAL_ERROR " VxWorks${CMAKE_SYSTEM_VERSION} types not found at ${VXWORKS_TYPES}.")
+endif()
+
+include_directories(SYSTEM "${VXWORKS_TYPES}")
