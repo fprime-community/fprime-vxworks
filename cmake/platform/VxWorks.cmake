f01ef68ee2cbc15b00c467f67e5f44fa5dbdb51c
Kevin F. Ortega <kevin.f.ortega@jpl.nasa.gov>
2024-06-27 22:25:16 -0700

    code cleanup

--- cmake/platform/VxWorks.cmake
+++ cmake/platform/VxWorks.cmake
@@ -1,9 +1,20 @@
-# Use VxWorks common file
-# Set VxWorks target
-add_definitions(-DTGT_OS_TYPE_VXWORKS)
-add_definitions(-D_WRS_KERNEL)
+####
+# VxWorks.cmake:
+#
+# VxWorks platform file for standard vxworks targets.
+####
+# Set platform default for baremetal scheduler drivers
+if (NOT DEFINED FPRIME_USE_BAREMETAL_SCHEDULER)
+   set(FPRIME_USE_BAREMETAL_SCHEDULER OFF)
+   #message(STATUS "Requiring thread library")
+   #FIND_PACKAGE ( Threads REQUIRED )
+endif()
+choose_fprime_implementation(Os/File Os/File/Stub)
+choose_fprime_implementation(Os/Task Os/Task/VxWorks)
 
-set(FPRIME_USE_BAREMETAL_SCHEDULE OFF)
+## VxWorks is mostly Posix compliant. So turn this on.
+#add_definitions(-D_POSIX_C_SOURCE)
+#set(FPRIME_USE_POSIX ON)
 
 # Add VxWorks specific headers into the system
 set(VXWORKS_TYPES "${CMAKE_CURRENT_LIST_DIR}/types/VxWorks${CMAKE_SYSTEM_VERSION}")
--- fprime-vxworks.cmake
+++ fprime-vxworks.cmake
@@ -1,2 +1,1 @@
-set(FPRIME_REQUIRED_VERSION "v3.1.1")
-add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/fprime-vxworks/VxWorks/")
+add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/VxWorks/Os")
