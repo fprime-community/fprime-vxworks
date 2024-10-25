e4f86533b0c29d488e189fe84a2d503722fed3dd
Kevin F Ortega <ortega@ip-100-65-57-213.jpl.nasa.gov>
2024-10-25 16:09:33 -0700

    formatting

--- VxWorks/Os/DefaultMutex.cpp
+++ VxWorks/Os/DefaultMutex.cpp
@@ -2,16 +2,16 @@
 // \title VxWorks/Os/DefaultMutex.cpp
 // \brief sets default Os::Mutex VxWorks implementation via linker
 // ======================================================================
-#include "Mutex.hpp"
 #include "Os/Delegate.hpp"
+#include "Os/Mutex.hpp"
+#include "VxWorks/Os/Mutex.hpp"
+
 namespace Os {
 
 //! \brief get a delegate for MutexInterface that intercepts calls for VxWorks
 //! \param aligned_new_memory: aligned memory to fill
 //! \return: pointer to delegate
-MutexInterface *MutexInterface::getDelegate(HandleStorage& aligned_new_memory) {
-    return Os::Delegate::makeDelegate<MutexInterface, Os::VxWorks::Mutex::VxWorksMutex>(
-            aligned_new_memory
-    );
+MutexInterface* MutexInterface::getDelegate(HandleStorage& aligned_new_memory) {
+    return Os::Delegate::makeDelegate<MutexInterface, Os::VxWorks::Mutex::VxWorksMutex>(aligned_new_memory);
 }
-}
+}  // namespace Os
--- VxWorks/Os/Mutex.cpp
+++ VxWorks/Os/Mutex.cpp
@@ -3,16 +3,16 @@
 // \brief VxWorks implementation for Os::Mutex
 // ======================================================================
 #include "VxWorks/Os/Mutex.hpp"
-#include "VxWorks/Os/error.hpp"
 #include <Fw/Types/Assert.hpp>
+#include "VxWorks/Os/error.hpp"
 
 namespace Os {
 namespace VxWorks {
 namespace Mutex {
 
 VxWorksMutex::VxWorksMutex() : Os::MutexInterface(), m_handle() {
-  this->m_handle.m_mutex_descriptor = semMCreate(SEM_Q_PRIORITY | SEM_INVERSION_SAFE | SEM_DELETE_SAFE);
-  FW_ASSERT(this->m_handle.m_mutex_descriptor != SEM_ID_NULL);
+    this->m_handle.m_mutex_descriptor = semMCreate(SEM_Q_PRIORITY | SEM_INVERSION_SAFE | SEM_DELETE_SAFE);
+    FW_ASSERT(this->m_handle.m_mutex_descriptor != SEM_ID_NULL);
 }
 
 VxWorksMutex::~VxWorksMutex() {
@@ -20,13 +20,13 @@ VxWorksMutex::~VxWorksMutex() {
 }
 
 VxWorksMutex::Status VxWorksMutex::take() {
-  STATUS status = semTake(this->m_handle.m_mutex_descriptor, WAIT_FOREVER);
-  return Os::VxWorks::vxworks_status_to_mutex_status(status);
+    STATUS status = semTake(this->m_handle.m_mutex_descriptor, WAIT_FOREVER);
+    return Os::VxWorks::vxworks_status_to_mutex_status(status);
 }
 
 VxWorksMutex::Status VxWorksMutex::release() {
-  STATUS status = semGive(this->m_handle.m_mutex_descriptor);
-  return Os::VxWorks::vxworks_status_to_mutex_status(status);
+    STATUS status = semGive(this->m_handle.m_mutex_descriptor);
+    return Os::VxWorks::vxworks_status_to_mutex_status(status);
 }
 
 MutexHandle* VxWorksMutex::getHandle() {
