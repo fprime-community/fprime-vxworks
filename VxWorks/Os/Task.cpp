85b160b0e5ea9ce2f05d18233b6e11b9aa297a82
Kevin F. Ortega <kevin.f.ortega@jpl.nasa.gov>
2024-10-25 17:28:39 -0700

    Updating task osal and cleaning it up

--- VxWorks/Os/Task.cpp
+++ VxWorks/Os/Task.cpp
@@ -1,11 +1,11 @@
-#include <vxWorks.h>
-#include <taskLib.h> // need it for VX_FP_TASK
 #include <sysLib.h>
+#include <taskLib.h>  // need it for VX_FP_TASK
+#include <vxWorks.h>
 
-#include <cstring>
 #include <unistd.h>
-#include <climits>
 #include <cerrno>
+#include <climits>
+#include <cstring>
 
 #include "Fw/Types/Assert.hpp"
 #include "Fw/Types/BasicTypes.hpp"
@@ -17,202 +17,100 @@ namespace Os {
 namespace VxWorks {
 namespace Task {
 
-    //!< This wrapper is needed due to the change in FUNCPTR's signature
-    //!< from VxWorks6 to VxWorks7. This static function implements the
-    //!< FUNCPTR signature required by VxWorks7 and calls the routine
-    //!< with the provides arg. This wrapper function is expected to be
-    //!< passed in to taskCreate.
-    static int myRoutineWrapper(
-        Os::TaskInterface::taskRoutine routine, //!< Routine that should run in a thread
-        void* arg //!< Argument passed to the routine
-    )
-    {
-        routine(arg);
-        return VXWORKS_OK;
-    }
-
-    void VxWorksTask::onStart() {}
-
-    Os::Task::Status VxWorksTask::start(const Arguments& arguments) {
-        FW_ASSERT(arguments.m_routine != nullptr);
-
-        // Get taskName into a non-const variable because that is what
-        // VxWorks' taskCreate wants.
-        char taskName[arguments.m_name.getCapacity()];
-        memcpy(taskName, arguments.m_name.toChar(), arguments.m_name.getCapacity());
-
-        // convert priority from Posix range to VxWorks range
-        PlatformIntType vxPriority = 255 - arguments.m_priority;
-
-        TASK_ID taskId = taskCreate(
-            taskName,
-            vxPriority,
-            VX_FP_TASK,
-            arguments.m_stackSize,
-            reinterpret_cast<FUNCPTR>(myRoutineWrapper),
-            0,0,0,0,0,0,0,0,0,0);
-//TODO
-            //reinterpret_cast<int>(arguments.m_routine_argument),0,0,0,0,0,0,0,0,0);
-        
-        if (TASK_ID_NULL == taskId) {
-            return Os::Task::Status::UNKNOWN_ERROR; 
-        }
-        
-        this->m_handle.m_task_descriptor = taskId;
-        
-#ifdef _WRS_CONFIG_SMP
-        if (arguments.m_cpuAffinity != -1) {
-            cpuset_t aff;
-
-            CPUSET_ZERO (aff);
-            CPUSET_SET (aff, arguments.m_cpuAffinity);
-
-	    if (taskCpuAffinitySet (this->m_handle.m_task_descriptor, aff) == ERROR) {
-               /* Either CPUs are not enabled or we are in UP mode */
-               taskDelete (this->m_handle.m_task_descriptor);
-               return Os::Task::Status::INVALID_AFFINITY;
-	    }
-
-        }
-#endif
-
-        auto status = taskActivate(taskId); // activate task
-        if (ERROR == status) {
-            return Os::Task::Status::UNKNOWN_ERROR; 
-        }
-
-        return Os::Task::Status::OP_OK;
-    }
-
-    Os::Task::Status VxWorksTask::join() {
-        // There is no task join in taskLib.
-        return Os::Task::Status::JOIN_ERROR;;
-    }
-
-    TaskHandle* VxWorksTask::getHandle() {
-        return &this->m_handle;
-    }
-
-    void VxWorksTask::suspend(Os::Task::SuspensionType /*suspensionType*/) {
-        STATUS status = taskSuspend(this->m_handle.m_task_descriptor);
-        FW_ASSERT(status == VXWORKS_OK, status);
-    }
-
-    void VxWorksTask::resume() {
-        STATUS status = taskResume(this->m_handle.m_task_descriptor);
-        FW_ASSERT(status == OK, status);
-    }
-} // end namespace Task
-} // end namespace VxWorks
-} // end namespace Os
-
-
-
-#if 0
-
-// old!!!!
-namespace Os {
-    Task::Task() : m_handle(0), m_identifier(0), m_affinity(-1), m_started(false), m_suspendedOnPurpose(false) {
-    }
-
-    Task::TaskStatus Task::start(const Fw::StringBase &name, taskRoutine routine, void* arg, NATIVE_UINT_TYPE priority, NATIVE_UINT_TYPE stackSize,  NATIVE_UINT_TYPE cpuAffinity, NATIVE_UINT_TYPE identifier) {
-
-        this->m_name = "TV_";
-        this->m_name += name;
-        this->m_identifier = identifier;
-        this->m_started = false;
-        this->m_affinity = cpuAffinity;
-        
-        // convert priority from Posix range to VxWorks range
-        NATIVE_INT_TYPE vxPriority = 255 - priority;
-        
-        // If a registry has been registered, register task
-        if (Task::s_taskRegistry) {
-            Task::s_taskRegistry->addTask(this);
-        }
-        
-        NATIVE_INT_TYPE stat = taskCreate((char*)this->m_name.toChar(),vxPriority,VX_FP_TASK,stackSize,(FUNCPTR)routine,(int)arg,0,0,0,0,0,0,0,0,0);
-        
-        if (ERROR == stat) {
-            return TASK_UNKNOWN_ERROR; 
-        }
-        
-        this->m_handle = (NATIVE_INT_TYPE)stat;
-        
-#ifdef _WRS_CONFIG_SMP
-        if (cpuAffinity != -1) {
-            cpuset_t aff;
-
-            CPUSET_ZERO (aff);
-            CPUSET_SET (aff, cpuAffinity);
-
-	    if (taskCpuAffinitySet (this->m_handle, aff) == ERROR) {
-               /* Either CPUs are not enabled or we are in UP mode */
-               taskDelete (this->m_handle);
-               return TASK_INVALID_AFFINITY;
-	    }
-
-        }
-#endif
-
-        stat = taskActivate(stat); // activate task
-        if (ERROR == stat) {
-            return TASK_UNKNOWN_ERROR;
-        }
-
-
-        Task::s_numTasks++;
-        
-        return TASK_OK;
-    
-    }
-    
-    Task::~Task() {
-        // If a registry has been registered, remove task
-        if (Task::s_taskRegistry) {
-            Task::s_taskRegistry->removeTask(this);
-        }
-        
-    }
-    
-    Task::TaskStatus Task::delay(NATIVE_UINT_TYPE milliseconds)
-    {
-
-        // Lookup the number of clock ticks for the specified
-        // number of milliseconds. Use the ticks to call the
-        // task delay function call.
-        Task::TaskStatus status = TASK_DELAY_ERROR;
-        
-        U32 ticks = ((milliseconds * sysClkRateGet() + 999) / 1000);
-        
-        if (taskDelay(ticks) == OK) {
-            status = TASK_OK;
-        }
-        return status;
-
-    }
-    
-    void Task::suspend(bool onPurpose) {
-        FW_ASSERT(taskSuspend(this->m_handle) == OK);
-    }
-                    
-    void Task::resume(void) {
-        FW_ASSERT(taskResume(this->m_handle) == OK);
-    }
-
-    bool Task::isSuspended(void) {
-        return (taskIsSuspended(this->m_handle) == TRUE)?true:false;
-    }
-
-    TaskId Task::getOsIdentifier(void) {
-      TaskId T; 
-      return T;
-    }
-
-    Task::TaskStatus Task::join(void **value_ptr) {
-        // No task join in taskLib.  Return error if used.
-        return TASK_JOIN_ERROR;
-    }
+//!< This static function implements the
+//!< FUNCPTR signature required by VxWorks7 and calls the routine
+//!< with the provides arg. This wrapper function is expected to be
+//!< passed in to taskCreate.
+static PlatformIntType myRoutineWrapper(
+    Os::TaskInterface::taskRoutine routine,  //!< Routine that should run in a thread
+    void* arg                                //!< Argument passed to the routine
+) {
+    routine(arg);
+    return OK;
 }
 
+void VxWorksTask::onStart() {}
+
+Os::Task::Status VxWorksTask::start(const Arguments& arguments) {
+    FW_ASSERT(arguments.m_routine != nullptr);
+
+    // Get taskName into a non-const variable because that is what
+    // VxWorks' taskCreate wants.
+    char taskName[arguments.m_name.getCapacity()];
+    memcpy(taskName, arguments.m_name.toChar(), arguments.m_name.getCapacity());
+
+    // convert priority from Posix range to VxWorks range
+    PlatformIntType vxPriority = 255 - arguments.m_priority;
+
+    this->m_handle.m_task_descriptor =
+        taskCreate(taskName, vxPriority, VX_FP_TASK, arguments.m_stackSize, reinterpret_cast<FUNCPTR>(myRoutineWrapper),
+                   0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    // TODO
+    // reinterpret_cast<int>(arguments.m_routine_argument),0,0,0,0,0,0,0,0,0);
+
+    if (this->m_handle.m_task_descriptor == TASK_ID_NULL) {
+        return Os::Task::Status::UNKNOWN_ERROR;
+    }
+
+#ifdef _WRS_CONFIG_SMP
+    if (arguments.m_cpuAffinity != -1) {
+        cpuset_t aff;
+
+        CPUSET_ZERO(aff);
+        CPUSET_SET(aff, arguments.m_cpuAffinity);
+
+        if (taskCpuAffinitySet(this->m_handle.m_task_descriptor, aff) == ERROR) {
+            /* Either CPUs are not enabled or we are in UP mode */
+            (void)taskDelete(this->m_handle.m_task_descriptor);
+            return Os::Task::Status::INVALID_AFFINITY;
+        }
+    }
 #endif
+
+    // Activate task
+    if (taskActivate(this->m_handle.m_task_descriptor) == ERROR) {
+        return Os::Task::Status::UNKNOWN_ERROR;
+    }
+
+    return Os::Task::Status::OP_OK;
+}
+
+Os::Task::Status VxWorksTask::join() {
+    // There is no task join in taskLib.
+    return Os::Task::Status::JOIN_ERROR;
+    ;
+}
+
+TaskHandle* VxWorksTask::getHandle() {
+    return &this->m_handle;
+}
+
+void VxWorksTask::suspend(Os::Task::SuspensionType /*suspensionType*/) {
+    STATUS status = taskSuspend(this->m_handle.m_task_descriptor);
+    FW_ASSERT(status == VXWORKS_OK, static_cast<FwAssertArgType>(status));
+}
+
+void VxWorksTask::resume() {
+    STATUS status = taskResume(this->m_handle.m_task_descriptor);
+    FW_ASSERT(status == OK, static_cast<FwAssertArgType>(status));
+}
+
+Os::Task::Status VxWOrksTask::_delay(Fw::TimeInterval interval) {
+    // Lookup the number of clock ticks for the specified
+    // number of milliseconds. Use the ticks to call the
+    // task delay function call.
+    Os::Task::Status status = Os::Task::DELAY_ERROR;
+
+    U32 milliseconds = interval.getUSeconds() / 1000 + interval.getSeconds() * 1000;
+    U32 ticks = ((milliseconds * sysClkRateGet() + 999) / 1000);
+
+    if (taskDelay(ticks) == OK) {
+        status = Os::Task::OP_OK;
+    }
+
+    return status;
+}
+
+}  // end namespace Task
+}  // end namespace VxWorks
+}  // end namespace Os
--- VxWorks/Os/Task.hpp
+++ VxWorks/Os/Task.hpp
@@ -5,99 +5,104 @@
 #ifndef OS_VXWORKS_TASK_HPP
 #define OS_VXWORKS_TASK_HPP
 
-
 // VxWorks headers
-#include <vxWorks.h>
-#include <taskLib.h> // need it for VX_FP_TASK
 #include <sysLib.h>
+#include <taskLib.h>  // need it for VX_FP_TASK
 #include <tickLib.h>
+#include <vxWorks.h>
 
 #include <atomic>
 
 #include <Os/Task.hpp>
 
 #include <FpConfig.hpp>
-#include <Fw/Types/Serializable.hpp>
-#include <Os/TaskString.hpp>
-#include <Os/Mutex.hpp>
 #include <Fw/Deprecate.hpp>
-
+#include <Fw/Types/Serializable.hpp>
+#include <Os/Mutex.hpp>
+#include <Os/TaskString.hpp>
 
 namespace Os {
 namespace VxWorks {
 namespace Task {
-    //! TaskHandle class definition for VxWorks implementations.
-    //!
-    struct VxWorksTaskHandle : public TaskHandle {
-        static constexpr PlatformIntType SUCCESS = 0;
+//! TaskHandle class definition for VxWorks implementations.
+//!
+struct VxWorksTaskHandle : public TaskHandle {
+    static constexpr PlatformIntType SUCCESS = 0;
 
-        //! VxWorks task descriptor
-        TASK_ID m_task_descriptor;
+    //! VxWorks task descriptor
+    TASK_ID m_task_descriptor = TASK_ID_NULL;
+};
+
+//! VxWorks task implementation as driven by pthreads implementation
+class VxWorksTask : public TaskInterface {
+  public:
+    //! Enumeration of permission expectations
+    enum PermissionExpectation {
+        EXPECT_PERMISSION,    //!< Expect that you hold necessary permissions
+        EXPECT_NO_PERMISSION  //!< Expect that you do not hold necessary permissions
     };
 
-    //! VxWorks task implementation as driven by pthreads implementation
-    class VxWorksTask : public TaskInterface {
-      public:
+    //! \brief default constructor
+    VxWorksTask() = default;
 
-        //! Enumeration of permission expectations
-        enum PermissionExpectation {
-            EXPECT_PERMISSION, //!< Expect that you hold necessary permissions
-            EXPECT_NO_PERMISSION //!< Expect that you do not hold necessary permissions
-        };
+    //! \brief default virtual destructor
+    ~VxWorksTask() = default;
 
-        //! \brief default constructor
-        VxWorksTask() = default;
+    //! \brief copy constructor is forbidden
+    VxWorksTask(const VxWorksTask& other) = delete;
 
-        //! \brief default virtual destructor
-        ~VxWorksTask() = default;
+    //! \brief assignment operator is forbidden
+    VxWorksTask& operator=(const VxWorksTask& other) = delete;
 
-        //! \brief copy constructor is forbidden
-        VxWorksTask(const VxWorksTask& other) = delete;
+    //! \brief perform required task start actions
+    void onStart() override;
 
-        //! \brief assignment operator is forbidden
-        VxWorksTask& operator=(const VxWorksTask& other) = delete;
+    //! \brief start the task
+    //!
+    //! Starts the task given the supplied arguments. This is done via the a task routine wrapper intermediary that
+    //! ensures that `setStarted` is called once the task has actually started to run. The task then runs the user
+    //! routine. This function may return before the new task begins to run.
+    //
+    //! It is illegal for arguments.m_routine to be null.
+    //!
+    //! \param arguments: arguments supplied to the task start call
+    //! \return status of the task start
+    Status start(const Arguments& arguments) override;
 
+    //! \brief block until the task has ended
+    //!
+    //! Blocks the current (calling) task until this task execution has ended. Callers should ensure that any
+    //! signals required to stop this task have already been emitted or will be emitted by another task.
+    //!
+    //! \return status of the block
+    Status join() override;  //!< Wait for task to finish
 
-        //! \brief perform required task start actions
-        void onStart() override;
+    //! \brief suspend the task given the suspension type
+    //!
+    //! suspensionType argument is not used.
+    //!
+    //! \param suspensionType intentionality of the suspension
+    void suspend(SuspensionType suspensionType) override;
 
-        //! \brief start the task
-        //!
-        //! Starts the task given the supplied arguments. This is done via the a task routine wrapper intermediary that
-        //! ensures that `setStarted` is called once the task has actually started to run. The task then runs the user
-        //! routine. This function may return before the new task begins to run.
-        //
-        //! It is illegal for arguments.m_routine to be null.
-        //!
-        //! \param arguments: arguments supplied to the task start call
-        //! \return status of the task start
-        Status start(const Arguments& arguments) override;
+    //! \brief resume a suspended task
+    //!
+    //! Resumes this task. Not started, running, and exited tasks take no action.
+    //!
+    void resume() override;
 
-        //! \brief block until the task has ended
-        //!
-        //! Blocks the current (calling) task until this task execution has ended. Callers should ensure that any
-        //! signals required to stop this task have already been emitted or will be emitted by another task.
-        //!
-        //! \return status of the block
-        Status join() override; //!< Wait for task to finish
+    //! \brief delay the current task
+    //!
+    //! Delays, or sleeps, the current task by the supplied time interval. In non-preempting os implementations
+    //! the task will resume no earlier than expected but an exact wake-up time is not guaranteed.
+    //!
+    //! \param interval: delay time
+    //! \return status of the delay
+    Status _delay(Fw::TimeInterval interval) override;
 
-        //! \brief suspend the task given the suspension type
-        //!
-        //! suspensionType argument is not used.
-        //!
-        //! \param suspensionType intentionality of the suspension
-        void suspend(SuspensionType suspensionType) override;
-
-        //! \brief resume a suspended task
-        //!
-        //! Resumes this task. Not started, running, and exited tasks take no action.
-        //!
-        void resume() override;
-
-        //! \brief return the underlying task handle (implementation specific)
-        //! \return internal task handle representation
-        TaskHandle* getHandle() override;
-      PRIVATE:
+    //! \brief return the underlying task handle (implementation specific)
+    //! \return internal task handle representation
+    TaskHandle* getHandle() override;
+    PRIVATE :
         //! \brief create a configured pthread
         //!
         //! Creates, and configures, but does not start a pthread. This may be called twice, once to try setting
@@ -106,12 +111,13 @@ namespace Task {
         //! \param arguments: arguments used to set priority, affinity, etc
         //! \param permissions: whether to expect permissions or not
         //! \return OP_OK on success, or an error
-        Status create(const Os::Task::Arguments& arguments, const VxWorksTask::PermissionExpectation permissions);
+        Status
+        create(const Os::Task::Arguments& arguments, const VxWorksTask::PermissionExpectation permissions);
 
-        VxWorksTaskHandle m_handle; //!< VxWorks task tracking
-        static std::atomic<bool> s_permissions_reported; //!< Permission errors have been reported
-    };
-} // end namespace Task
-} // end namespace VxWorks
-} // end namespace Os
+    VxWorksTaskHandle m_handle;                       //!< VxWorks task tracking
+    static std::atomic<bool> s_permissions_reported;  //!< Permission errors have been reported
+};
+}  // end namespace Task
+}  // end namespace VxWorks
+}  // end namespace Os
 #endif
