55ca7524c96585b9f53b4705c23247a9fde623aa
Kevin F. Ortega <kevin.f.ortega@jpl.nasa.gov>
2024-10-28 11:55:29 -0700

    Alphabetized

--- cmake/platform/vxworks-llvm.cmake
+++ cmake/platform/vxworks-llvm.cmake
@@ -4,14 +4,14 @@
 # VxWorks platform file for standard vxworks targets.
 ####
 
-choose_fprime_implementation(Os/File Os/File/Stub)
 choose_fprime_implementation(Os/Console Os/Console/VxWorks)
-choose_fprime_implementation(Os/Task Os/Task/VxWorks)
+choose_fprime_implementation(Os/Cpu Os/Cpu/Stub)
+choose_fprime_implementation(Os/File Os/File/Stub)
+choose_fprime_implementation(Os/Memory Os/Memory/Stub)
 choose_fprime_implementation(Os/Mutex Os/Mutex/VxWorks)
 choose_fprime_implementation(Os/Queue Os/Generic/PriorityQueue)
 choose_fprime_implementation(Os/RawTime Os/RawTime/Stub)
-choose_fprime_implementation(Os/Cpu Os/Cpu/Stub)
-choose_fprime_implementation(Os/Memory Os/Memory/Stub)
+choose_fprime_implementation(Os/Task Os/Task/VxWorks)
 
 # Add VxWorks specific headers into the system
 set(VXWORKS_TYPES "${CMAKE_CURRENT_LIST_DIR}/types")
