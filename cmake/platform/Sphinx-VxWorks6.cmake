73e5de079e56f8cc54ae08e818dd74caf82b2813
Kevin F. Ortega <kevin.f.ortega@jpl.nasa.gov>
2024-10-30 19:08:38 -0700

    cleaned up platform files

--- cmake/platform/Sphinx-VxWorks6.cmake
+++
@@ -1,4 +0,0 @@
-# Use VxWorks common file
-add_definitions(-DBUILD_SPHINX)
-include_directories($ENV{WIND_BASE}/target/h $ENV{WIND_BASE}/target/h/wrn/coreip)
-include("${CMAKE_CURRENT_LIST_DIR}/VxWorks-common.cmake")
