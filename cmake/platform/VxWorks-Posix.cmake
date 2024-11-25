a7c5be9efbb81de3b41276e3864cc715928ed218
Kevin F. Ortega <kevin.f.ortega@jpl.nasa.gov>
2024-11-25 14:14:10 -0800

    Enabling sockets so deployments can build with the F Prime provided Ip, Udp, Tcp stack. Also making VxWorks-Posix use Posix file instead of stubs

--- cmake/platform/VxWorks-Posix.cmake
+++ cmake/platform/VxWorks-Posix.cmake
@@ -1,13 +1,21 @@
-add_compile_definitions(TGT_OS_TYPE_VXWORKS)
+####
+# VxWorks-Posix.cmake:
+#
+# VxWorks posix platform file for standard vxworks targets.
+####
+
 choose_fprime_implementation(Os/Console Os/Console/Posix)
 choose_fprime_implementation(Os/Cpu Os/Cpu/Stub)
-choose_fprime_implementation(Os/File Os/File/Stub)
+choose_fprime_implementation(Os/File Os/File/Posix)
 choose_fprime_implementation(Os/Memory Os/Memory/Stub)
 choose_fprime_implementation(Os/Mutex Os/Mutex/Posix)
 choose_fprime_implementation(Os/Queue Os/Generic/PriorityQueue)
 choose_fprime_implementation(Os/RawTime Os/RawTime/Stub)
 choose_fprime_implementation(Os/Task Os/Task/Posix)
+
+add_compile_definitions(TGT_OS_TYPE_VXWORKS)
 set(FPRIME_USE_POSIX ON)
+set(FPRIME_HAS_SOCKETS ON)
 
 # Add VxWorks specific headers into the system
 include_directories(SYSTEM "${CMAKE_CURRENT_LIST_DIR}/types")
--- cmake/platform/VxWorks.cmake
+++ cmake/platform/VxWorks.cmake
@@ -4,7 +4,6 @@
 # VxWorks platform file for standard vxworks targets.
 ####
 
-add_compile_definitions(TGT_OS_TYPE_VXWORKS)
 choose_fprime_implementation(Os/Console Os/Console/VxWorks)
 choose_fprime_implementation(Os/Cpu Os/Cpu/Stub)
 choose_fprime_implementation(Os/File Os/File/Stub)
@@ -13,6 +12,9 @@ choose_fprime_implementation(Os/Mutex Os/Mutex/VxWorks)
 choose_fprime_implementation(Os/Queue Os/Generic/PriorityQueue)
 choose_fprime_implementation(Os/RawTime Os/RawTime/Stub)
 choose_fprime_implementation(Os/Task Os/Task/VxWorks)
+
+add_compile_definitions(TGT_OS_TYPE_VXWORKS)
+set(FPRIME_HAS_SOCKETS ON)
 
 # Add VxWorks specific headers into the system
 include_directories(SYSTEM "${CMAKE_CURRENT_LIST_DIR}/types")
