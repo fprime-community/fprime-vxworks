b1e4f7f6eadee7574dfed1891ae041fdfe2c64ab
Kevin F Ortega <kevin.f.ortega@jpl.nasa.gov>
2025-01-16 13:03:43 -0800

    Adding missing conditional variable interface

--- VxWorks/Os/DefaultMutex.cpp
+++ VxWorks/Os/DefaultMutex.cpp
@@ -3,7 +3,7 @@
 // \brief sets default Os::Mutex VxWorks implementation via linker
 // ======================================================================
 #include "Os/Delegate.hpp"
-#include "Os/Mutex.hpp"
+#include "VxWorks/Os/ConditionVariable.hpp"
 #include "VxWorks/Os/Mutex.hpp"
 
 namespace Os {
@@ -13,5 +13,14 @@ namespace Os {
 //! \return: pointer to delegate
 MutexInterface* MutexInterface::getDelegate(MutexHandleStorage& aligned_new_memory) {
     return Os::Delegate::makeDelegate<MutexInterface, Os::VxWorks::Mutex::VxWorksMutex>(aligned_new_memory);
+}
+
+//! \brief get a delegate for MutexInterface that intercepts calls for VxWorks
+//! \param aligned_new_memory: aligned memory to fill
+//! \return: pointer to delegate
+ConditionVariableInterface* ConditionVariableInterface::getDelegate(
+    ConditionVariableHandleStorage& aligned_new_memory) {
+    return Os::Delegate::makeDelegate<ConditionVariableInterface, Os::VxWorks::Mutex::VxWorksConditionVariable,
+                                      ConditionVariableHandleStorage>(aligned_new_memory);
 }
 }  // namespace Os
