a975746d704efcc3bf31e21c96b45c7844c16fa3
Kevin F Ortega <kevin.f.ortega@jpl.nasa.gov>
2025-02-19 17:35:04 -0800

    fixed constant name

--- VxWorks/Os/Console.cpp
+++ VxWorks/Os/Console.cpp
@@ -14,12 +14,12 @@ namespace Console {
 void VxWorksConsole::writeMessage(const CHAR* message, const FwSizeType size) {
     if (message != nullptr) {
         FwIndexType m_tail_index = this->m_handle.m_tail_index;
-        FW_ASSERT(m_tail_index < CONSOLE_CAPACITY, m_tail_index, CONSOLE_CAPACITY);
-        FwSizeType minSize = FW_MIN(size, CONSOLE_MESSAGE_SIZE);
+        FW_ASSERT(m_tail_index < MAX_CONSOLE_CAPACITY, m_tail_index, MAX_CONSOLE_CAPACITY);
+        FwSizeType minSize = FW_MIN(size, MAX_CONSOLE_MESSAGE_BYTE_SIZE);
         (void)memcpy(this->m_handle.circularBuffer[m_tail_index], message, minSize);
         this->m_handle.circularBuffer[m_tail_index][minSize] = '\0';
         logMsg(this->m_handle.circularBuffer[m_tail_index], 0, 0, 0, 0, 0, 0);
-        m_tail_index = ((m_tail_index + 1) % CONSOLE_CAPACITY);
+        m_tail_index = ((m_tail_index + 1) % MAX_CONSOLE_CAPACITY);
         this->m_handle.m_tail_index = m_tail_index;
     }
 }
