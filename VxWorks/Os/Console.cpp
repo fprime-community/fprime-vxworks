68f002e805798457b1d6ccf53c37065e63d40003
Kevin F Ortega <kevin.f.ortega@jpl.nasa.gov>
2025-02-27 16:38:39 -0800

    bugfix regarding index

--- VxWorks/Os/Console.cpp
+++ VxWorks/Os/Console.cpp
@@ -19,7 +19,7 @@ void VxWorksConsole::writeMessage(const CHAR* message, const FwSizeType size) {
         const FwSizeType currentIndex = this->m_handle.m_tail_index.fetch_add(1) % MAX_CONSOLE_CAPACITY;
         FW_ASSERT(currentIndex < MAX_CONSOLE_CAPACITY, static_cast<FwAssertArgType>(currentIndex),
                   static_cast<FwAssertArgType>(MAX_CONSOLE_CAPACITY));
-        FwSizeType minSize = FW_MIN(size, MAX_CONSOLE_MESSAGE_BYTE_SIZE);
+        FwSizeType minSize = FW_MIN(size, MAX_CONSOLE_MESSAGE_BYTE_SIZE - 1);
         (void)memcpy(this->m_handle.circularBuffer[currentIndex], message, minSize);
         this->m_handle.circularBuffer[currentIndex][minSize] = '\0';
         (void)logMsg(this->m_handle.circularBuffer[currentIndex], 0, 0, 0, 0, 0, 0);
