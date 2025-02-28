475749bbbc06a31b21974f97749925e3da0886b3
Kevin F Ortega <kevin.f.ortega@jpl.nasa.gov>
2025-02-27 23:13:49 -0800

    Maded component passive as it should be

--- Components/VxWatchDogTimer/VxWatchDogTimer.cpp
+++ Components/VxWatchDogTimer/VxWatchDogTimer.cpp
@@ -13,13 +13,13 @@ namespace Components {
 // ----------------------------------------------------------------------
 
 VxWatchDogTimer ::VxWatchDogTimer(const char* const compName) : VxWatchDogTimerComponentBase(compName) {
-    m_watchdogId = wdCreate();
+    this->m_watchdogId = wdCreate();
 }
 
 VxWatchDogTimer ::~VxWatchDogTimer() {
-    if (m_watchdogId != nullptr) {
-        (void)wdCancel(m_watchdogId);
-        (void)wdDelete(m_watchdogId);
+    if (this->m_watchdogId != nullptr) {
+        (void)wdCancel(this->m_watchdogId);
+        (void)wdDelete(this->m_watchdogId);
     }
 }
 
@@ -29,44 +29,18 @@ void VxWatchDogTimer::callIsr(U32 ticks) {
 }
 
 void VxWatchDogTimer::s_driverISR(void* arg) {
-    FW_ASSERT(arg);
+    FW_ASSERT(arg != nullptr);
     // cast argument to component instance
     VxWatchDogTimer* compPtr = static_cast<VxWatchDogTimer*>(arg);
-    compPtr->InterruptReport_internalInterfaceInvoke(0);
-    FW_ASSERT(compPtr->m_watchdogId != nullptr);
-    STATUS status = wdStart(compPtr->m_watchdogId, compPtr->m_tickDelay, reinterpret_cast<FUNCPTR>(s_driverISR),
-                            reinterpret_cast<_Vx_usr_arg_t>(compPtr));
-    FW_ASSERT(status == VXWORKS_OK);
-}
-// ----------------------------------------------------------------------
-// Handler implementations for typed input ports
-// ----------------------------------------------------------------------
-
-void VxWatchDogTimer ::BufferIn_handler(FwIndexType portNum, Drv::DataBuffer& buff) {
-    // just a pass-through
-    this->BufferOut_out(0, buff);
-}
-
-void VxWatchDogTimer ::PingIn_handler(FwIndexType portNum, U32 key) {
-    // call ping output port
-    this->PingOut_out(0, key);
-}
-
-void VxWatchDogTimer ::Sched_handler(FwIndexType portNum, U32 context) {}
-
-// ----------------------------------------------------------------------
-// Handler implementations for user-defined internal interfaces
-// ----------------------------------------------------------------------
-
-void VxWatchDogTimer ::InterruptReport_internalInterfaceHandler(U32 interrupt) {
     // get time
     Os::RawTime time;
     time.now();
     // call output timing signal
-    this->CycleOut_out(0, time);
-    // increment cycles and write channel
-    this->tlmWrite_Cycles(this->m_cycles);
-    this->m_cycles++;
+    compPtr->CycleOut_out(0, time);
+    FW_ASSERT(compPtr->m_watchdogId != nullptr);
+    STATUS status = wdStart(compPtr->m_watchdogId, compPtr->m_tickDelay, reinterpret_cast<FUNCPTR>(s_driverISR),
+                            reinterpret_cast<_Vx_usr_arg_t>(compPtr));
+    FW_ASSERT(status == VXWORKS_OK);
 }
 
 }  // namespace Components
--- Components/VxWatchDogTimer/VxWatchDogTimer.hpp
+++ Components/VxWatchDogTimer/VxWatchDogTimer.hpp
@@ -28,52 +28,15 @@ class VxWatchDogTimer : public VxWatchDogTimerComponentBase {
     void callIsr(U32 ticks);
 
     PRIVATE :
-        // ----------------------------------------------------------------------
-        // Handler implementations for typed input ports
-        // ----------------------------------------------------------------------
 
-        //! Handler implementation for BufferIn
-        //!
-        //! The input data buffer port
-        void
-        BufferIn_handler(FwIndexType portNum,   //!< The port number
-                         Drv::DataBuffer& buff  //!< A data buffer
-                         ) override;
-
-    //! Handler implementation for PingIn
-    //!
-    //! Input ping port
-    void PingIn_handler(FwIndexType portNum,  //!< The port number
-                        U32 key               //!< Value to return to pinger
-                        ) override;
-
-    //! Handler implementation for Sched
-    //!
-    //! The rate group scheduler input
-    void Sched_handler(FwIndexType portNum,  //!< The port number
-                       U32 context           //!< The call order
-                       ) override;
-
-    PRIVATE :
-        // ----------------------------------------------------------------------
-        // Handler implementations for user-defined internal interfaces
-        // ----------------------------------------------------------------------
-
-        //! Handler implementation for InterruptReport
-        //!
-        //! Internal interrupt reporting interface
-        void
-        InterruptReport_internalInterfaceHandler(U32 interrupt  //!< The interrupt register value
-                                                 ) override;
-
-    // static ISR callback
-    static void s_driverISR(void* arg);
+        // static ISR callback
+        static void
+        s_driverISR(void* arg);
 
     // ----------------------------------------------------------------------
     // Member variable
     // ----------------------------------------------------------------------
     WDOG_ID m_watchdogId = nullptr;
-    U32 m_cycles = 0;
     U32 m_tickDelay = 0;
 };
 
