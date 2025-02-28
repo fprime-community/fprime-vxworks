a7b476ae829be1e08eae0c5b1673207b7ad972ba
Kevin F Ortega <kevin.f.ortega@jpl.nasa.gov>
2025-02-27 16:41:45 -0800

    Adding static assert

--- VxWorks/Os/Console.cpp
+++ VxWorks/Os/Console.cpp
@@ -15,6 +15,7 @@ void VxWorksConsole::writeMessage(const CHAR* message, const FwSizeType size) {
     if (message != nullptr) {
         static_assert(std::is_unsigned<FwSizeType>::value, "FwSizeType is expected to be unsigned.");
         static_assert(MAX_CONSOLE_CAPACITY > 0, "Avoid dividing by zero.");
+        static_assert(MAX_CONSOLE_MESSAGE_BYTE_SIZE > 0, "Should not have a message size of 0 bytes."
         // Rely on unsigned overflow to atomically roll over tail index
         const FwSizeType currentIndex = this->m_handle.m_tail_index.fetch_add(1) % MAX_CONSOLE_CAPACITY;
         FW_ASSERT(currentIndex < MAX_CONSOLE_CAPACITY, static_cast<FwAssertArgType>(currentIndex),
