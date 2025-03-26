33333635b6fd937ddc60a5d4be356e0f7ccf8cad
Nhat Nguyen <nhat.nguyen@jpl.nasa.gov>
2025-03-26 12:09:25 -0500

    Move VxWatchDogTimer from fprime-vxworks-reference. (#24)
    * renaming component to a more clear name of what it does
    
    * Maded component passive as it should be
    
    * code cleanup. moving start of watchdog to a startwatchdog function in component which is public and can be used by users to start the watchdog.
    
    * add function to stop watchdog
    
    * Asserting watchdog id is created before starting it
    
    * updated component per review
    
    * conversion typo
    
    * Move to new Vxworks/Svc folder.
    
    * Update files with new namespace and path.
    
    * Add svc directory to cmake.
    
    * Add missing CMakeList.txt to Svc folder and fix path in VxWatchDogTimer.fpp
    
    ---------
    
    Co-authored-by: Kevin F Ortega <kevin.f.ortega@jpl.nasa.gov>

---
+++ VxWorks/Svc/VxWatchDogTimer/VxWatchDogTimer.fpp
@@ -0,0 +1,11 @@
+module VxWorksSvc {
+    @ VxWorks WatchDog Timer
+    passive component VxWatchDogTimer {
+
+    # ----------------------------------------------------------------------
+    # General ports
+    # ----------------------------------------------------------------------
+
+    include "../../../../fprime/Drv/Interfaces/TickInterface.fppi"
+    }
+}
---
+++ VxWorks/Svc/VxWatchDogTimer/VxWatchDogTimer.hpp
@@ -0,0 +1,49 @@
+// ======================================================================
+// \title  VxWatchDogTimer.hpp
+// \author ortega
+// \brief  hpp file for VxWatchDogTimer component implementation class
+// ======================================================================
+
+#ifndef SVC_VXWORKS_VXWATCHDOGTIMER_HPP
+#define SVC_VXWORKS_VXWATCHDOGTIMER_HPP
+
+#include <wdLib.h>
+#include "VxWorks/Svc/VxWatchDogTimer/VxWatchDogTimerComponentAc.hpp"
+
+namespace VxWorksSvc {
+
+class VxWatchDogTimer : public VxWatchDogTimerComponentBase {
+  public:
+    // ----------------------------------------------------------------------
+    // Component construction and destruction
+    // ----------------------------------------------------------------------
+
+    //! Construct VxWatchDogTimer object
+    VxWatchDogTimer(const char* const compName  //!< The component name
+    );
+
+    //! Destroy VxWatchDogTimer object
+    ~VxWatchDogTimer();
+
+    void startWatchdog(U32 ticks);
+
+    void startWatchdog(Fw::TimeInterval interval);
+
+    void stopWatchdog();
+
+    PRIVATE :
+
+        // static ISR callback
+        static void
+        s_driverISR(void* arg);
+
+    // ----------------------------------------------------------------------
+    // Member variable
+    // ----------------------------------------------------------------------
+    WDOG_ID m_watchdogId = nullptr;
+    _Vx_ticks_t m_tickDelay = 0;
+};
+
+}  // namespace VxWorksSvc
+
+#endif
