ce9e59c2e0e4c7afd012a5456c3b2a2b87f65bbf
kevin-f-ortega <kevin.f.ortega@jpl.nasa.gov>
2025-06-18 18:31:16 -0700

    upgrading to latest in fprime4 (#28)
    * upgrading to latest in fprime4
    
    * clean up to docs
    
    * updated needed by latest fprime

--- cmake/platform/VxWorks-Posix.cmake
+++ cmake/platform/VxWorks-Posix.cmake
@@ -5,17 +5,23 @@
 ####
 register_fprime_target("${CMAKE_CURRENT_LIST_DIR}/../target/vxvalidate.cmake")
 
-choose_fprime_implementation(Os/Console Os/Console/Posix)
-choose_fprime_implementation(Os/Cpu Os/Cpu/Stub)
-choose_fprime_implementation(Os/File Os/File/Posix)
-choose_fprime_implementation(Os/Memory Os/Memory/Stub)
-choose_fprime_implementation(Os/Mutex Os/Mutex/Posix)
-choose_fprime_implementation(Os/Queue Os/Generic/PriorityQueue)
-choose_fprime_implementation(Os/RawTime Os/RawTime/Posix)
-choose_fprime_implementation(Os/Task Os/Task/Posix)
-
-add_compile_definitions(TGT_OS_TYPE_VXWORKS)
 set(FPRIME_USE_POSIX ON)
 set(FPRIME_HAS_SOCKETS ON)
-
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/types/Platform")
+
+register_fprime_config(
+        PlatformVxWorksPosix
+   INTERFACE # No buildable files generated
+   CHOOSES_IMPLEMENTATIONS
+        Os_Console_Posix
+        Os_File_Posix
+        Os_Mutex_Posix
+        Os_Generic_PriorityQueue
+        Os_RawTime_Posix
+        Os_Task_Posix
+        Fw_StringFormat_snprintf
+        # No VxWorks Implementation
+        Os_Cpu_Stub
+        Os_Memory_Stub
+)
+target_compile_definitions(PlatformVxWorksPosix INTERFACE -DTGT_OS_TYPE_VXWORKS)
