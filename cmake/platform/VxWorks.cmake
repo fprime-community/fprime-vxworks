c3c314917245b295db7e4d09c39b85f8a438645e
kevin-f-ortega <kevin.f.ortega@jpl.nasa.gov>
2025-06-30 16:57:57 -0700

    upgrading to fprime4 (#29)

--- cmake/platform/VxWorks.cmake
+++ cmake/platform/VxWorks.cmake
@@ -22,5 +22,7 @@ register_fprime_config(
         Os_File_Stub
         Os_Memory_Stub
         Os_RawTime_Stub
+   BASE_CONFIG
 )
+
 target_compile_definitions(PlatformVxWorks INTERFACE -DTGT_OS_TYPE_VXWORKS)
