1af249ab40acc1c0d7628114fe32d8965a2f1bc3
Kevin F Ortega <kevin.f.ortega@jpl.nasa.gov>
2025-08-01 12:02:25 -0700

    enhance overflow check to avoid using u64

--- VxWorks/Svc/VxWatchDogTimer/VxWatchDogTimer.cpp
+++ VxWorks/Svc/VxWatchDogTimer/VxWatchDogTimer.cpp
@@ -24,23 +24,38 @@ VxWatchDogTimer ::~VxWatchDogTimer() {
     }
 }
 
-void VxWatchDogTimer::startTimer(U32 ticks) {
-    this->m_tickDelay = static_cast<_Vx_ticks_t>(ticks);
+void VxWatchDogTimer::startTimer(FwSizeType milliseconds) {
+    // Calculate ticks per interval by multiplying interval by number of ticks per second, then round up.
+    // Make sure we do not overflow
+    // ms * rate + (ms_p_sec - 1) <= max
+    FW_ASSERT(milliseconds != 0);
+    FW_ASSERT(sysClkRateGet() <= ((std::numeric_limits<_Vx_ticks_t>::max() - (MS_PER_SECS - 1)) / milliseconds),
+              static_cast<FwAssertArgType>(sysClkRateGet()), static_cast<FwAssertArgType>(milliseconds));
+    this->m_tickDelay = ((milliseconds * sysClkRateGet()) + (MS_PER_SECS - 1)) / MS_PER_SECS;
+    this->startTimerTick(this->m_tickDelay);
+}
+
+void VxWatchDogTimer::startTimer(Fw::TimeInterval interval) {
+    // Avoid overflow when converting seconds to milliseconds
+    // seconds*ms_p_sec <= max
+    // FW_ASSERT(interval.getSeconds() <= (std::numeric_limits<FwSizeType>::max() / MS_PER_SECS),
+    FW_ASSERT(MS_PER_SECS <= (std::numeric_limits<FwSizeType>::max() / interval.getSeconds()),
+              static_cast<FwAssertArgType>(interval.getSeconds()));
+    // Avoid overflow when converting interval to milliseconds
+    // seconds*ms_p_sec + usecs/us_p_ms <= max
+    FW_ASSERT((interval.getSeconds() * MS_PER_SECS) <=
+                  (std::numeric_limits<FwSizeType>::max() - (interval.getUSeconds() / USECS_PER_MS)),
+              static_cast<FwAssertArgType>(interval.getSeconds()),
+              static_cast<FwAssertArgType>(interval.getUSeconds()));
+    FwSizeType milliseconds = (interval.getSeconds() * MS_PER_SECS) + (interval.getUSeconds() / USECS_PER_MS);
+    this->startTimer(milliseconds);
+}
+
+void VxWatchDogTimer::startTimerTick(_Vx_ticks_t ticks) {
     FW_ASSERT(this->m_watchdogId != nullptr);
     STATUS status = wdStart(this->m_watchdogId, this->m_tickDelay, reinterpret_cast<FUNCPTR>(s_driverISR),
                             reinterpret_cast<_Vx_usr_arg_t>(this));
     FW_ASSERT(status == VXWORKS_OK);
-}
-
-void VxWatchDogTimer::startTimer(Fw::TimeInterval interval) {
-    static constexpr U32 MS_PER_SECS = 1000;
-    static constexpr U32 USECS_PER_MS = 1000;
-    U64 delayInMs = (interval.getSeconds() * MS_PER_SECS) + (interval.getUSeconds() / USECS_PER_MS);
-    FW_ASSERT(delayInMs <= std::numeric_limits<U32>::max(), static_cast<FwAssertArgType>(delayInMs));
-    // Calculate ticks per interval by multiplying interval by number of ticks per second, then round up.
-    U64 ticksPerInterval = (((delayInMs * sysClkRateGet()) + (MS_PER_SECS - 1)) / MS_PER_SECS);
-    FW_ASSERT(ticksPerInterval <= std::numeric_limits<U32>::max(), static_cast<FwAssertArgType>(ticksPerInterval));
-    this->startTimer(ticksPerInterval);
 }
 
 void VxWatchDogTimer::quit() {
@@ -64,7 +79,7 @@ void VxWatchDogTimer::s_driverISR(void* arg) {
     compPtr->CycleOut_out(0, time);
 
     // Start watchdog timer again
-    compPtr->startTimer(compPtr->m_tickDelay);
+    compPtr->startTimerTick(compPtr->m_tickDelay);
 }
 
 }  // namespace VxWorksSvc
--- VxWorks/Svc/VxWatchDogTimer/VxWatchDogTimer.hpp
+++ VxWorks/Svc/VxWatchDogTimer/VxWatchDogTimer.hpp
@@ -7,6 +7,7 @@
 #ifndef SVC_VXWORKS_VXWATCHDOGTIMER_HPP
 #define SVC_VXWORKS_VXWATCHDOGTIMER_HPP
 
+#include <sysLib.h>
 #include <wdLib.h>
 #include "VxWorks/Svc/VxWatchDogTimer/VxWatchDogTimerComponentAc.hpp"
 
@@ -29,15 +30,19 @@ class VxWatchDogTimer : public VxWatchDogTimerComponentBase {
 
     void startTimer(Fw::TimeInterval interval);
 
+    void startTimer(FwSizeType milliseconds);
+
+    void startTimerTick(_Vx_ticks_t ticks);
+
   private:
     // static ISR callback
     static void s_driverISR(void* arg);
 
-    void startTimer(U32 ticks);
-
     // ----------------------------------------------------------------------
     // Member variable
     // ----------------------------------------------------------------------
+    static constexpr U32 MS_PER_SECS = 1000;
+    static constexpr U32 USECS_PER_MS = 1000;
     WDOG_ID m_watchdogId = nullptr;
     _Vx_ticks_t m_tickDelay = 0;
 };
