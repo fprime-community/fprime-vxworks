7403566e0f69febb8b031e191f2aaf7603e30491
Kevin F. Ortega <kevin.f.ortega@jpl.nasa.gov>
2025-10-16 17:33:25 -0700

    formatting

--- VxWorks/Svc/VxWatchDogTimer/VxWatchDogTimer.cpp
+++ VxWorks/Svc/VxWatchDogTimer/VxWatchDogTimer.cpp
@@ -29,7 +29,8 @@ void VxWatchDogTimer::startTimer(FwSizeType microseconds) {
     // Avoid overflow
     // usecs * clkRate + (us_p_s - 1) <= max
     // clkRate <= (max - (us_p_s - 1)) / usecs
-    FW_ASSERT(sysClkRateGet() <= (std::numeric_limits<_Vx_ticks_t>::max() - (USECS_PER_SECS - 1)) / microseconds, static_cast<FwAssertArgType>(sysClkRateGet()), static_cast<FwAssertArgType>(microseconds));
+    FW_ASSERT(sysClkRateGet() <= (std::numeric_limits<_Vx_ticks_t>::max() - (USECS_PER_SECS - 1)) / microseconds,
+              static_cast<FwAssertArgType>(sysClkRateGet()), static_cast<FwAssertArgType>(microseconds));
     this->m_tickDelay = ((microseconds * sysClkRateGet()) + (USECS_PER_SECS - 1)) / USECS_PER_SECS;
     this->startTimerTick(this->m_tickDelay);
 }
@@ -38,7 +39,9 @@ void VxWatchDogTimer::startTimer(Fw::TimeInterval interval) {
     // Avoid overflow when converting interval to microseconds
     // (seconds * us_p_ms * ms_p_s) + usecs <= max
     // seconds <= (max - usecs) / (us_p_ms * ms_p_s)
-    FW_ASSERT(interval.getSeconds() <= (std::numeric_limits<FwSizeType>::max() - interval.getUSeconds()) / USECS_PER_SECS, static_cast<FwAssertArgType>(interval.getSeconds()), static_cast<FwAssertArgType>(interval.getUSeconds()));
+    FW_ASSERT(
+        interval.getSeconds() <= (std::numeric_limits<FwSizeType>::max() - interval.getUSeconds()) / USECS_PER_SECS,
+        static_cast<FwAssertArgType>(interval.getSeconds()), static_cast<FwAssertArgType>(interval.getUSeconds()));
     FwSizeType microseconds = (interval.getSeconds() * USECS_PER_SECS) + interval.getUSeconds();
     this->startTimer(microseconds);
 }
