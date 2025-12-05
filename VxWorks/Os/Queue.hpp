dd4aff5785905a3202bffe30ff1beac0ad166558
kevin-f-ortega <kevin.f.ortega@jpl.nasa.gov>
2025-12-05 11:16:59 -0800

    Upgrade to fprime4.1 (#38)
    * updated queue interface to match new fprime queue interface
    
    * marking argument as unused

--- VxWorks/Os/Queue.hpp
+++ VxWorks/Os/Queue.hpp
@@ -46,7 +46,7 @@ class VxWorksQueue : public QueueInterface {
     //! \param depth: depth of queue in number of messages
     //! \param messageSize: size of an individual message
     //! \return: status of the creation
-    Status create(const Fw::ConstStringBase& name, FwSizeType depth, FwSizeType messageSize) override;
+    Status create(FwEnumStoreType id, const Fw::ConstStringBase& name, FwSizeType depth, FwSizeType messageSize) override;
 
     //! \brief send a message into the queue
     //!
