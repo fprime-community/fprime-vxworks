29f6a9369d91ace423e266930960cf02f72f86ef
kevin-f-ortega <kevin.f.ortega@jpl.nasa.gov>
2025-12-18 13:38:26 -0800

    Handling default task values (#39)
    * formatting
    
    * asserting that we are not using TASK_DEFAULT for stack size. TASK_DEFAULT would result in the max value of a U64, which is invalid for a stack size. Having the assert here allows users, who accidentally use the default stack size, to quickly catch the error.
    
    * replaced assert with return status. much better to do this
    
    * explicitly handling default priority handling
    
    * need static cast
    
    * return error when the default priority is used. We want users to explicitly set the values
    
    * returning correct status

--- VxWorks/Os/Task.cpp
+++ VxWorks/Os/Task.cpp
@@ -21,9 +21,8 @@ namespace Task {
 //!< FUNCPTR signature required by VxWorks7 and calls the routine
 //!< with the provides arg. This wrapper function is expected to be
 //!< passed in to taskCreate.
-static int myRoutineWrapper(
-    Os::TaskInterface::taskRoutine routine,  //!< Routine that should run in a thread
-    void* arg                                //!< Argument passed to the routine
+static int myRoutineWrapper(Os::TaskInterface::taskRoutine routine,  //!< Routine that should run in a thread
+                            void* arg                                //!< Argument passed to the routine
 ) {
     routine(arg);
     return OK;
@@ -40,6 +39,14 @@ Os::Task::Status VxWorksTask::start(const Arguments& arguments) {
     auto minimumStringSize = FW_MIN(sizeof(taskName), arguments.m_name.getCapacity());
     memcpy(taskName, arguments.m_name.toChar(), minimumStringSize);
 
+    if (arguments.m_stackSize == Os::Task::TASK_DEFAULT) {
+        return Os::Task::Status::INVALID_STACK;
+    }
+
+    if (arguments.m_priority == Os::Task::TASK_PRIORITY_DEFAULT) {
+        return Os::Task::Status::INVALID_PRIORITY;
+    }
+
     this->m_handle.m_task_descriptor = taskCreate(
         taskName, static_cast<int>(arguments.m_priority), VX_FP_TASK, static_cast<size_t>(arguments.m_stackSize),
         reinterpret_cast<FUNCPTR>(myRoutineWrapper), reinterpret_cast<_Vx_usr_arg_t>(arguments.m_routine),
@@ -50,6 +57,7 @@ Os::Task::Status VxWorksTask::start(const Arguments& arguments) {
     }
 
 #ifdef _WRS_CONFIG_SMP
+    // when cpuAffinity is set to TASK_DEFAULT, the affinity will be assigned by the scheduler
     if (arguments.m_cpuAffinity != Os::Task::TASK_DEFAULT) {
         cpuset_t aff;
 
