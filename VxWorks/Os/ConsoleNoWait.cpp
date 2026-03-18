e688c74fb0c9ac2f2cb278433a080bf9523fa0c6
Kevin F. Ortega <kevin.f.ortega@jpl.nasa.gov>
2026-03-18 16:24:39 -0700

    removing some code duplication

--- VxWorks/Os/ConsoleNoWait.cpp
+++ VxWorks/Os/ConsoleNoWait.cpp
@@ -2,16 +2,16 @@
 // \title VxWorks/Os/ConsoleNoWait.cpp
 // \brief VxWorks implementation for Os::Console using logMsgNoWait
 // ======================================================================
-#include "ConsoleNoWait.hpp"
+#include "Console.hpp"
 #include <logLib.h>
 #include <Fw/Types/Assert.hpp>
 #include <cstring>
 
 namespace Os {
 namespace VxWorks {
-namespace ConsoleNoWait {
+namespace Console {
 
-void VxWorksConsoleNoWait::writeMessage(const CHAR* message, const FwSizeType size) {
+void VxWorksConsole::writeMessage(const CHAR* message, const FwSizeType size) {
     if (message != nullptr) {
         static_assert(std::is_unsigned<FwSizeType>::value, "FwSizeType is expected to be unsigned.");
         static_assert(MAX_CONSOLE_CAPACITY > 0, "Avoid dividing by zero.");
@@ -23,14 +23,12 @@ void VxWorksConsoleNoWait::writeMessage(const CHAR* message, const FwSizeType si
         FwSizeType minSize = FW_MIN(size, MAX_CONSOLE_MESSAGE_BYTE_SIZE - 1);
         (void)memcpy(this->m_handle.circularBuffer[currentIndex], message, minSize);
         this->m_handle.circularBuffer[currentIndex][minSize] = '\0';
+
+        // logMsgNoWait() will not block when called from a task or when called from an ISR.
         (void)logMsgNoWait(this->m_handle.circularBuffer[currentIndex], 0, 0, 0, 0, 0, 0);
     }
 }
 
-ConsoleHandle* VxWorksConsoleNoWait::getHandle() {
-    return &this->m_handle;
-}
-
-}  // namespace ConsoleNoWait
+}  // namespace Console
 }  // namespace VxWorks
 }  // namespace Os
