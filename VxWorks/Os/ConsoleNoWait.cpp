4c97228575c49d76eada6f22d1f1dda281e7bb92
Kevin F Ortega <kevin.f.ortega@jpl.nasa.gov>
2026-03-19 13:29:37 -0700

    code cleanup

--- VxWorks/Os/ConsoleNoWait.cpp
+++ VxWorks/Os/ConsoleNoWait.cpp
@@ -2,16 +2,17 @@
 // \title VxWorks/Os/ConsoleNoWait.cpp
 // \brief VxWorks implementation for Os::Console using logMsgNoWait
 // ======================================================================
-#include "Console.hpp"
 #include <logLib.h>
 #include <Fw/Types/Assert.hpp>
-#include <cstring>
+#include "Console.hpp"
 
 namespace Os {
 namespace VxWorks {
 namespace Console {
 
 void VxWorksConsole::myLogMsg(const FwSizeType currentIndex) {
+    FW_ASSERT(currentIndex < MAX_CONSOLE_CAPACITY, static_cast<FwAssertArgType>(currentIndex),
+              static_cast<FwAssertArgType>(MAX_CONSOLE_CAPACITY));
     // logMsgNoWait() will not block when called from a task or when called from an ISR.
     (void)logMsgNoWait(this->m_handle.circularBuffer[currentIndex], 0, 0, 0, 0, 0, 0);
 }
--- VxWorks/Os/ConsoleWait.cpp
+++ VxWorks/Os/ConsoleWait.cpp
@@ -2,16 +2,17 @@
 // \title VxWorks/Os/Console.cpp
 // \brief VxWorks implementation for Os::Console using logMsg
 // ======================================================================
-#include "Console.hpp"
 #include <logLib.h>
 #include <Fw/Types/Assert.hpp>
-#include <cstring>
+#include "Console.hpp"
 
 namespace Os {
 namespace VxWorks {
 namespace Console {
 
 void VxWorksConsole::myLogMsg(const FwSizeType currentIndex) {
+    FW_ASSERT(currentIndex < MAX_CONSOLE_CAPACITY, static_cast<FwAssertArgType>(currentIndex),
+              static_cast<FwAssertArgType>(MAX_CONSOLE_CAPACITY));
     // logMsg() checks to see whether or not it is running in interrupt context.
     // If it is, it will not block. However, if invoked from a task, it can cause
     // the task to block.
