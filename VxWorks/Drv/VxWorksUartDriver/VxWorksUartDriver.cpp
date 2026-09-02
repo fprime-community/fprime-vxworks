7b8129223383466c24a68ccd48c8f20ea100a050
Timothy Canham <timothy.canham@jpl.nasa.gov>
2026-09-02 14:45:48 -0700

    Adding VxWorks UART

---
+++ VxWorks/Drv/VxWorksUartDriver/VxWorksUartDriver.cpp
@@ -0,0 +1,248 @@
+// ======================================================================
+// \title  VxWorksUartDriverImpl.cpp
+// \author tcanham
+// \brief  cpp file for VxWorksUartDriver component implementation class
+//
+// \copyright
+// Copyright 2009-2015, by the California Institute of Technology.
+// ALL RIGHTS RESERVED.  United States Government Sponsorship
+// acknowledged.
+//
+// ======================================================================
+
+#include <VxWorks/Drv/VxWorksUartDriver/VxWorksUartDriver.hpp>
+#include <Os/TaskString.hpp>
+
+#include "Fw/Types/BasicTypes.hpp"
+
+#include <fcntl.h>
+#include <ioLib.h>
+#include <sioLib.h>
+#include <termios.h>
+#include <selectLib.h>
+#include <cerrno>
+#include <cstring>
+
+namespace Drv {
+
+// ----------------------------------------------------------------------
+// Construction, initialization, and destruction
+// ----------------------------------------------------------------------
+
+VxWorksUartDriver ::VxWorksUartDriver(const char* const compName)
+    : VxWorksUartDriverComponentBase(compName),
+      m_fd(-1),
+      m_allocationSize(0),
+      m_device("NOT_EXIST"),
+      m_bytesSent(0),
+      m_bytesReceived(0),
+      m_quitReadThread(false) {}
+
+bool VxWorksUartDriver::open(const char* const device,
+                              UartBaudRate baud,
+                              UartFlowControl fc,
+                              UartParity parity,
+                              FwSizeType allocationSize) {
+    FW_ASSERT(device != nullptr);
+    int fd = -1;
+    int stat = -1;
+    this->m_allocationSize = allocationSize;
+
+    this->m_device = device;
+
+    fd = ::open(device, O_RDWR, 0);
+
+    if (fd == -1) {
+        Fw::LogStringArg _arg = device;
+        Fw::LogStringArg _err = strerror(errno);
+        this->log_WARNING_HI_OpenError(_arg, this->m_fd, _err);
+        return false;
+    }
+
+    this->m_fd = fd;
+
+    // Set baud rate using ioctl
+    stat = ::ioctl(fd, SIO_BAUD_SET, static_cast<int>(baud));
+    if (stat == -1) {
+        ::close(fd);
+        Fw::LogStringArg _arg = device;
+        Fw::LogStringArg _err = strerror(errno);
+        this->log_WARNING_HI_OpenError(_arg, fd, _err);
+        return false;
+    }
+
+    // Set hardware options: 8 data bits, 1 stop bit
+    int hwOptions = CS8 | CREAD | CLOCAL;
+
+    // Set parity
+    switch (parity) {
+        case PARITY_ODD:
+            hwOptions |= (PARENB | PARODD);
+            break;
+        case PARITY_EVEN:
+            hwOptions |= PARENB;
+            break;
+        case PARITY_NONE:
+            break;
+        default:
+            FW_ASSERT(0, parity);
+            break;
+    }
+
+    stat = ::ioctl(fd, SIO_HW_OPTS_SET, hwOptions);
+    if (stat == -1) {
+        ::close(fd);
+        Fw::LogStringArg _arg = device;
+        Fw::LogStringArg _err = strerror(errno);
+        this->log_WARNING_HI_OpenError(_arg, fd, _err);
+        return false;
+    }
+
+    // Set flow control
+    if (fc == HW_FLOW) {
+        stat = ::ioctl(fd, SIO_HW_OPTS_SET, hwOptions | CRTSCTS);
+        if (stat == -1) {
+            ::close(fd);
+            Fw::LogStringArg _arg = device;
+            Fw::LogStringArg _err = strerror(errno);
+            this->log_WARNING_HI_OpenError(_arg, fd, _err);
+            return false;
+        }
+    }
+
+    // Set to raw mode (non-canonical)
+    stat = ::ioctl(fd, FIOSETOPTIONS, OPT_RAW);
+    if (stat == -1) {
+        ::close(fd);
+        Fw::LogStringArg _arg = device;
+        Fw::LogStringArg _err = strerror(errno);
+        this->log_WARNING_HI_OpenError(_arg, fd, _err);
+        return false;
+    }
+
+    // All done!
+    Fw::LogStringArg _arg = device;
+    this->log_ACTIVITY_HI_PortOpened(_arg);
+    if (this->isConnected_ready_OutputPort(0)) {
+        this->ready_out(0);
+    }
+    return true;
+}
+
+VxWorksUartDriver ::~VxWorksUartDriver() {
+    if (this->m_fd != -1) {
+        (void)::close(this->m_fd);
+    }
+}
+
+// ----------------------------------------------------------------------
+// Handler implementations for user-defined typed input ports
+// ----------------------------------------------------------------------
+
+void VxWorksUartDriver ::run_handler(FwIndexType portNum, U32 context) {
+    this->tlmWrite_BytesSent(this->m_bytesSent);
+    this->tlmWrite_BytesRecv(this->m_bytesReceived);
+}
+
+Drv::ByteStreamStatus VxWorksUartDriver ::send_handler(const FwIndexType portNum, Fw::Buffer& serBuffer) {
+    Drv::ByteStreamStatus status = Drv::ByteStreamStatus::OP_OK;
+    if (this->m_fd == -1 || serBuffer.getData() == nullptr || serBuffer.getSize() == 0) {
+        status = Drv::ByteStreamStatus::OTHER_ERROR;
+    } else {
+        unsigned char* data = serBuffer.getData();
+        FW_ASSERT_NO_OVERFLOW(serBuffer.getSize(), size_t);
+        size_t xferSize = static_cast<size_t>(serBuffer.getSize());
+
+        ssize_t stat = ::write(this->m_fd, data, xferSize);
+
+        if (-1 == stat || static_cast<size_t>(stat) != xferSize) {
+            Fw::LogStringArg _arg = this->m_device;
+            this->log_WARNING_HI_WriteError(_arg, static_cast<I32>(stat));
+            status = Drv::ByteStreamStatus::OTHER_ERROR;
+        } else {
+            this->m_bytesSent += static_cast<FwSizeType>(stat);
+        }
+    }
+    return status;
+}
+
+void VxWorksUartDriver::recvReturnIn_handler(FwIndexType portNum, Fw::Buffer& fwBuffer) {
+    this->deallocate_out(0, fwBuffer);
+}
+
+void VxWorksUartDriver ::serialReadTaskEntry(void* ptr) {
+    FW_ASSERT(ptr != nullptr);
+    Drv::ByteStreamStatus status = ByteStreamStatus::OTHER_ERROR;
+    VxWorksUartDriver* comp = reinterpret_cast<VxWorksUartDriver*>(ptr);
+    while (!comp->m_quitReadThread) {
+        Fw::Buffer buff = comp->allocate_out(0, comp->m_allocationSize);
+
+        if (buff.getData() == nullptr) {
+            Fw::LogStringArg _arg = comp->m_device;
+            comp->log_WARNING_HI_NoBuffers(_arg);
+            status = ByteStreamStatus::OTHER_ERROR;
+            comp->recv_out(0, buff, status);
+            Os::Task::delay(Fw::TimeInterval(0, 50000));
+            continue;
+        }
+
+        int stat = 0;
+
+        FW_ASSERT_NO_OVERFLOW(buff.getSize(), size_t);
+
+        // Use select to implement timeout
+        fd_set readfds;
+        struct timeval timeout;
+
+        FD_ZERO(&readfds);
+        FD_SET(comp->m_fd, &readfds);
+
+        timeout.tv_sec = 1;
+        timeout.tv_usec = 0;
+
+        int selectRet = ::select(comp->m_fd + 1, &readfds, NULL, NULL, &timeout);
+
+        if (selectRet > 0 && FD_ISSET(comp->m_fd, &readfds)) {
+            stat = static_cast<int>(::read(comp->m_fd, buff.getData(), static_cast<size_t>(buff.getSize())));
+        } else if (selectRet == 0) {
+            stat = 0;
+        } else {
+            stat = -1;
+        }
+
+        buff.setSize(0);
+
+        if (stat == -1) {
+            Fw::LogStringArg _arg = comp->m_device;
+            comp->log_WARNING_HI_ReadError(_arg, stat);
+            status = ByteStreamStatus::OTHER_ERROR;
+        } else if (stat > 0) {
+            buff.setSize(static_cast<U32>(stat));
+            status = ByteStreamStatus::OP_OK;
+            comp->m_bytesReceived += static_cast<FwSizeType>(stat);
+        } else {
+            status = ByteStreamStatus::OTHER_ERROR;
+        }
+
+        comp->recv_out(0, buff, status);
+    }
+}
+
+void VxWorksUartDriver ::start(FwTaskPriorityType priority,
+                                Os::Task::ParamType stackSize,
+                                Os::Task::ParamType cpuAffinity) {
+    Os::TaskString task("SerReader");
+    Os::Task::Arguments arguments(task, serialReadTaskEntry, this, priority, stackSize, cpuAffinity);
+    Os::Task::Status stat = this->m_readTask.start(arguments);
+    FW_ASSERT(stat == Os::Task::OP_OK, stat);
+}
+
+void VxWorksUartDriver ::quitReadThread() {
+    this->m_quitReadThread = true;
+}
+
+Os::Task::Status VxWorksUartDriver ::join() {
+    return m_readTask.join();
+}
+
+}  // end namespace Drv
---
+++ VxWorks/Drv/VxWorksUartDriver/VxWorksUartDriver.hpp
@@ -0,0 +1,115 @@
+// ======================================================================
+// \title  VxWorksUartDriverImpl.hpp
+// \author tcanham
+// \brief  hpp file for VxWorksUartDriver component implementation class
+//
+// \copyright
+// Copyright 2009-2015, by the California Institute of Technology.
+// ALL RIGHTS RESERVED.  United States Government Sponsorship
+// acknowledged.
+//
+// ======================================================================
+
+#ifndef VxWorksUartDriver_HPP
+#define VxWorksUartDriver_HPP
+
+#include <VxWorks/Drv/VxWorksUartDriver/VxWorksUartDriverComponentAc.hpp>
+#include <Os/Mutex.hpp>
+#include <Os/Task.hpp>
+
+#include <atomic>
+
+namespace Drv {
+
+class VxWorksUartDriver final : public VxWorksUartDriverComponentBase {
+  public:
+    // ----------------------------------------------------------------------
+    // Construction, initialization, and destruction
+    // ----------------------------------------------------------------------
+
+    //! Construct object VxWorksUartDriver
+    //!
+    VxWorksUartDriver(const char* const compName /*!< The component name*/
+    );
+
+    //! Configure UART parameters
+    enum UartBaudRate {
+        BAUD_9600 = 9600,
+        BAUD_19200 = 19200,
+        BAUD_38400 = 38400,
+        BAUD_57600 = 57600,
+        BAUD_115K = 115200,
+        BAUD_230K = 230400,
+        BAUD_460K = 460800,
+        BAUD_921K = 921600
+    };
+
+    enum UartFlowControl { NO_FLOW, HW_FLOW };
+
+    enum UartParity { PARITY_NONE, PARITY_ODD, PARITY_EVEN };
+
+    // Open device with specified baud and flow control.
+    bool open(const char* const device,
+              UartBaudRate baud,
+              UartFlowControl fc,
+              UartParity parity,
+              FwSizeType allocationSize);
+
+    //! start the serial poll thread.
+    //! buffSize is the max receive buffer size
+    //!
+    void start(FwTaskPriorityType priority = Os::Task::TASK_PRIORITY_DEFAULT,
+               Os::Task::ParamType stackSize = Os::Task::TASK_DEFAULT,
+               Os::Task::ParamType cpuAffinity = Os::Task::TASK_DEFAULT);
+
+    //! Quit thread
+    void quitReadThread();
+
+    //! Join thread
+    Os::Task::Status join();
+
+    //! Destroy object VxWorksUartDriver
+    //!
+    ~VxWorksUartDriver();
+
+  private:
+    // ----------------------------------------------------------------------
+    // Handler implementations for user-defined typed input ports
+    // ----------------------------------------------------------------------
+
+    //! Handler implementation for run
+    //!
+    //! The rate group input for sending telemetry
+    void run_handler(FwIndexType portNum,  //!< The port number
+                     U32 context           //!< The call order
+                     ) override;
+
+    //! Handler implementation for serialSend
+    //!
+    Drv::ByteStreamStatus send_handler(FwIndexType portNum, /*!< The port number*/
+                                       Fw::Buffer& serBuffer) override;
+
+    //! Handler implementation for recvReturnIn
+    //!
+    //! Port receiving back ownership of data sent out on $recv port
+    void recvReturnIn_handler(FwIndexType portNum,  //!< The port number
+                              Fw::Buffer& fwBuffer  //!< The buffer
+                              ) override;
+
+    int m_fd;                     //!< file descriptor returned for I/O device
+    FwSizeType m_allocationSize;  //!< size of allocation request to memory manager
+    const char* m_device;         //!< original device path
+
+    //! This method will be called by the new thread to wait for input on the serial port.
+    static void serialReadTaskEntry(void* ptr);
+
+    Os::Task m_readTask;  //!< task instance for thread to read serial port
+
+    std::atomic<FwSizeType> m_bytesSent;      //!< number of bytes sent
+    std::atomic<FwSizeType> m_bytesReceived;  //!< number of bytes received
+    bool m_quitReadThread;                    //!< flag to quit thread
+};
+
+}  // end namespace Drv
+
+#endif
