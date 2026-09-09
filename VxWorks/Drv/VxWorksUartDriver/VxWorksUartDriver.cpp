0c1a1a56015859153a2be58147ae72de77ec6f6d
Timothy Canham <timothy.canham@jpl.nasa.gov>
2026-09-08 19:47:57 -0700

    Shortened the task name

--- VxWorks/Drv/VxWorksUartDriver/VxWorksUartDriver.cpp
+++ VxWorks/Drv/VxWorksUartDriver/VxWorksUartDriver.cpp
@@ -232,7 +232,7 @@ void VxWorksUartDriver ::start(FwTaskPriorityType priority,
                                 Os::Task::ParamType stackSize,
                                 Os::Task::ParamType cpuAffinity) {
     Os::TaskString task;
-    task.format("VUD_%s", this->getObjName());
+    task.format("%s", this->getObjName());
     Os::Task::Arguments arguments(task, serialReadTaskEntry, this, priority, stackSize, cpuAffinity);
     Os::Task::Status stat = this->m_readTask.start(arguments);
     FW_ASSERT(stat == Os::Task::OP_OK, stat);
